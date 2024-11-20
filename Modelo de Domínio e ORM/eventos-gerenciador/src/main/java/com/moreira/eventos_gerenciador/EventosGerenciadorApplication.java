3552dff8a0bc43b9171d958d938a855b7601e28e
Nathan Moreira <nathangc7@gmail.com>
2024-11-20 09:22:43 -0300

    Primeiro commit eventos-gerenciador

---
+++ Modelo de Domínio e ORM/eventos-gerenciador/src/main/java/com/moreira/eventos_gerenciador/EventosGerenciadorApplication.java
@@ -0,0 +1,13 @@
+package com.moreira.eventos_gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosGerenciadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosGerenciadorApplication.class, args);
+	}
+
+}
---
+++ Modelo de Domínio e ORM/eventos-gerenciador/src/test/java/com/moreira/eventos_gerenciador/EventosGerenciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreira.eventos_gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosGerenciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
